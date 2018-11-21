3c561983e9d7599ec96f0823bccced8d43de567a
Samuel Ramirez <samuel_ramirez@live.com.mx>
2018-11-21 12:16:28 -0600

    REST SERVER -usuario

--- server/server.js
+++ server/server.js
@@ -1,46 +1,24 @@
 require('./config/config');
 
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const express     = require('express');
+const mongoose    = require('mongoose');
+const bodyParser  = require('body-parser');
+
+const app         = express();
+
 
 //midleware
 app.use(bodyParser.urlencoded({ extended:false }));
 app.use(bodyParser.json());
 
 
-app.get('/', function (req, res) {
-  res.json('Hello World');
-});
+app.use( require('./routes/usuario') );
 
-app.get('/usuario', function (req, res) {
-  res.json('GET Usuario');
-});
 
-app.post('/usuario', function (req, res) {
-  let body = req.body;
+mongoose.connect( process.env.URLDB, (err, res) => {
+  if (err)  throw err;
 
-  if ( body.nombre === undefined ) {
-    res.status(400).json({
-      ok: false,
-      mensaje: 'El nombre es necesario'
-    });
-  } else {
-    res.json({
-      persona: body
-    });
-  }
-
-});
-
-app.put('/usuario/:id', function (req, res) {
-  let id = req.params.id;
-
-  res.json({id});
-});
-
-app.delete('/usuario', function (req, res) {
-  res.json('DELETE Usuario');
+  console.log('Base de datos ONLINE');
 });
 
 
